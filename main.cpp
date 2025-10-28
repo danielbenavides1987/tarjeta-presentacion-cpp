79711d121154a581a0b0579b0564173f1dd69efc
Daniel Benavides <profesionalbencor@gmail.com>
2025-10-28 18:14:34 -0400

    Tarjeta de presentacion - primera version

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Tarjeta de presentacion digital - C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "***********************************************" << endl;
+    cout << "* TARJETA DE PRESENTACION                     *" << endl;
+    cout << "* Nombre: Daniel Alberto Benavides Cordido    *" << endl;
+    cout << "* Edad: 38 anos                               *" << endl;
+    cout << "* Intereses:                                  *" << endl;
+    cout << "*  - tecnologia, finanzas, videojuegos        *" << endl;
+    cout << "*  - futbol, mundo fitness                    *" << endl;
+    cout << "*  - comida thai y mediterranea               *" << endl;
+    cout << "* Bienvenido a mi mundo de superacion         *" << endl;
+    cout << "***********************************************" << endl;
+
+    return 0;
+}
+//  mente positiva siempre, sere un programador profesional.
+/*Un día a la vez,
+ * tengo un desafio más,
+ * aunque mi cuerpo quiere tumbarme ,
+ * sere imparable
+ * gracias AO por este curso, por la oportunidad y por creer en mi*/
+
